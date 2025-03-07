2b84ad49a02845c21b20f2a71f3129f9c3ddc1e8
Sasmith Manawadu <sasmithgit@gmail.com>
2025-03-07 09:20:04 +0530

    add shadcn/ui | integrate Prisma | implement dashboard

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,15 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <h1>hello from the index page</h1>
+    <div className="py-6">
+        <h1 className="text-3xl font-bold tracking-tight mb-8">Latest Posts</h1>
+
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+
+
+        </div>
+    </div>
   );
 }
