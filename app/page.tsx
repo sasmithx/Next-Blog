e6e13311ed4dafa4354bc96a83f15218f8251a5a
Sasmith Manawadu <sasmithgit@gmail.com>
2025-03-08 20:41:29 +0530

    Streaming Suspense Completed

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 import {prisma} from "@/app/util/db";
+import {BlogPostCard} from "@/components/general/BlogPostCard";
+import {Suspense} from "react";
 
 async function getData() {
-
+    await new Promise((resolve) => setTimeout(resolve, 2000));
     const data = await prisma.blogPost.findMany({
         select: {
             title: true,
@@ -11,24 +13,34 @@ async function getData() {
             authorName: true,
             id: true,
             createdAt: true,
-        }
+            authorId: true,
+            updatedAt: true,
+        },
     });
 
     return data;
 }
 
-export default async function Home() {
-  const data = await getData();
-  return (
-    <div className="py-6">
-        <h1 className="text-3xl font-bold tracking-tight mb-8">Latest Posts</h1>
+export default function Home() {
+    return (
+        <div className="py-6">
+            <h1 className="text-3xl font-bold tracking-tight mb-8">Latest Posts</h1>
 
+            <Suspense fallback={<p>Hello Waiting...</p>}>
+                <BlogPosts/>
+            </Suspense>
+        </div>
+    );
+}
+
+async function BlogPosts() {
+    const data = await getData();
+
+    return (
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-
             {data.map((item) => (
-                <h1 key={item.title}>{item.title}</h1>
+                <BlogPostCard data={item} key={item.id}/>
             ))}
         </div>
-    </div>
-  );
+    );
 }
