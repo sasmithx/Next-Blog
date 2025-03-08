f1f7fa3d756befa67e92bbc4cf8c65184a6d3af1
Sasmith Manawadu <sasmithgit@gmail.com>
2025-03-08 14:19:10 +0530

    Middleware Completed | Create Post UI Completed

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,8 +1,17 @@
-export default function DashboardRoute() {
+import Link from "next/link";
+import {buttonVariants} from "@/components/ui/button";
+
+export default async function DashboardRoute() {
+
     return (
         <div>
-            <h1>Dashboard</h1>
-            <p>Welcome from the dashboard!</p>
+            <div className="flex items-center justify-between mb-4">
+                <h2 className="text-xl font-medium">Your Blog Articles</h2>
+
+                <Link className={buttonVariants()} href="/dashboard/create">
+                    Create Post
+                </Link>
+            </div>
         </div>
     );
 }
