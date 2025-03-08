d099db4847d5e9a9add7a401ffedf8b748350414
Sasmith Manawadu <sasmithgit@gmail.com>
2025-03-08 22:37:39 +0530

    assets updated

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,12 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
+      // Add Freepik configuration
+      {
+        hostname: "img.freepik.com",
+        protocol: "https",
+        port: "",
+      },
       {
         hostname: "gravatar.com",
         protocol: "https",
